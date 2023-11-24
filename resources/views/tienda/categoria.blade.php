f3f91e0525a15c50add7bd4f68eee51a67c95d79
Douni28 <douglasjimenez999@gmail.com>
2023-11-24 10:15:55 -0600

    ignorar
    fghjk

--- resources/views/tienda/categoria.blade.php
+++ resources/views/tienda/categoria.blade.php
@@ -8,7 +8,11 @@
       <button class="btn-close p-4 position-absolute top-0 end-0 z-index-20 shadow-0" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
       <div class="modal-body p-0">
         <div class="row align-items-stretch">
-          <div class="col-lg-6 p-lg-0"><a class=" img-fluid glightbox product-view d-block h-100 bg-cover bg-center" style="background: url(img/{{$item->img}})" href="img/{{$item->img}}" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a><a class="glightbox d-none" href="img/product-5-alt-1.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a><a class="glightbox d-none" href="img/product-5-alt-2.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a></div>
+          <div class="col-lg-6 p-lg-0" style="background: url(img/{{$item->img}}) center center / cover no-repeat;">
+            <a class="img-fluid w-100 glightbox product-view d-block h-100" href="img/{{$item->img}}" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a>
+            <a class="glightbox d-none" href="img/product-5-alt-1.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a>
+            <a class="glightbox d-none" href="img/product-5-alt-2.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a>
+        </div>
           <div class="col-lg-6">
             <div class="p-4 my-md-4">
               <ul class="list-inline mb-2">
@@ -21,7 +25,6 @@
               <h2 class="h4">{{$item->nombre}}</h2>
               <p class="text-muted">${{$item->precio}}</p>
               <p class="text-sm mb-4">{{$item->descripcion}}</p>
-                     
             </div>
           </div>
         </div>
@@ -36,7 +39,7 @@
                   <div class="col-lg-4 col-sm-6">
                     <div class="product text-center">
                       <div class="mb-3 position-relative">
-                        <div class="badge text-white bg-"></div><a class="d-block" href="detail.html"><img class="img-fluid w-100" src="img/{{$item->img}}" alt="..." style="    height: 291px;"></a>
+                        <div class="badge text-white bg-"></div><a class="d-block" href=""><img class="img-fluid w-100" src="img/{{$item->img}}" alt="..." style="    height: 291px;"></a>
                         <div class="product-overlay">
                           <ul class="mb-0 list-inline">
                             <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-outline-dark" href="#!"><i class="far fa-heart"></i></a></li>
